13fc780269ad330555634b0c45496ef7e659e2a5
Aryan Kumar <arya9kumar9@gmail.com>
2025-12-08 21:51:08 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,7 +18,7 @@ export default function Home() {
   const [passwordError, setPasswordError] = useState("")
 
   // Simple password protection
-  const correctPassword = "arkadevs"
+  const correctPassword = "varnika26"
 
   useEffect(() => {
     setMounted(true)
