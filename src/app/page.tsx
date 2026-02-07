20aace6a495842c1970c553a9269fc75919ddb03
Aryan Kumar <arya9kumar9@gmail.com>
2026-02-07 15:06:42 +0530

    Change correct password for authentication

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,7 +18,7 @@ export default function Home() {
   const [passwordError, setPasswordError] = useState("")
 
   // Simple password protection
-  const correctPassword = "varnika26"
+  const correctPassword = "v"
 
   useEffect(() => {
     setMounted(true)
